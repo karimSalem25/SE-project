72a2ab58b30f9a1a6bc672a4879dcfc12a805de9
Shahenda-Magdy <shahendamagdymohammed54@gmail.com>
2021-06-24 20:35:17 +0200

    shahendas login

--- server/index.js
+++ server/index.js
@@ -24,3 +24,84 @@ console.log('Connection is established and running on port: ' + PORT)
 )).catch((err) => console.log(err.message));
 
 mongoose.set('useFindAndModify' , false);
+
+var passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose"),
+    User = require("./models/user");
+
+const app = express()
+app.set('view-engine', 'ejs')
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema ({})
+
+
+UserSchema.methods.authenticate = function(password) {
+  //implementation code goes here
+}
+
+//mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect("mongodb+srv://Shahenda Magdy:shosho1942001@cluster0.zf4ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+app.use(express.urlencoded({extended: false}))
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.get('/',(req,res) => {
+    res.render('f.ejs')
+})
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+app.get("/secret", isLoggedIn, function (req, res) {
+    res.render("secret");
+});
+ 
+app.get('/register',(req,res) => {
+    res.render('register.ejs')
+})
+
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    var email = req.body.email
+    User.register(new User({ username: username }),
+            password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+ 
+        passport.authenticate("local")(
+            req, res, function () {
+            res.render("secret");
+        });
+    });
+});
+app.get('/login',(req,res) => {
+    res.render('login.ejs')
+})
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
