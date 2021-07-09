db7d9ad431a55a5f12c8086c6f9cc4eb9d643976
karimSalem25 <karim.salem@student.giu-uni.de>
2021-07-09 20:16:17 +0200

    fixed
    amr user story fixed

--- client/src/TAview.js
+++ client/src/TAview.js
@@ -1,6 +1,6 @@
-//import logo from './logo.svg';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import Student from './components/TAviewStudent/TAviewStudent.js';
+import Create from './components/apply/apply.js';
 import useStyles from './styles'
 
 
@@ -11,21 +11,21 @@ function TAview() {
       <Container maxWidth = "g"> 
         <AppBar className={classes.appBar} position="static" color="inherit">
           <Typography className={classes.heading} variant = "h2" align = "center">
-            Show students
-            </Typography>  
+            View Students
+            </Typography>
          </AppBar>
         <Grow in>
           <Container>
             <Grid container justify = "space-between" alignItems = "stretch">
               <Grid item xs = {12} sm = {7}>
                   <appBar className = {classes.appBar} position="static" color="inherit">
-                    <Student />
+                  <Student />
                   </appBar>
               </Grid>
             </Grid>
               <Grid item xs = {12} sm = {4}>
               <appBar className = {classes.appBar} position="static" color="inherit">
-                    
+                     
                   </appBar>
               </Grid>
           </Container>
--- client/src/components/TAviewStudent/TAviewStudent.js
+++ client/src/components/TAviewStudent/TAviewStudent.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import axios from "axios";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
+import IconButton from '@material-ui/core/IconButton'; 
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 const useStyles = makeStyles({
   table: {
@@ -17,58 +18,64 @@ const useStyles = makeStyles({
   },
 });
 
+
 export default function TAShowStudent() {
   const classes = useStyles();
 
-  const [TAstudentsList, TAsetStudentList] = useState([]);
+  const [TAstudentsList, TAsetStudentList] = useState([])
 
-const deleteStudent = (id) => {
-    axios.delete('http://localhost:5000/viewS' + id).then(() => {
-        window.location.reload(false); 
+  const deleteStudent= (id) => {
+    axios.delete(`http://localhost:5000/apply/${id}`).then( () =>{
+      window.location.reload(false); 
     })
-}
-    
-  useEffect(() => {
-    axios.get("http://localhost:5000/viewS").then((allStudents) => {
-        TAsetStudentList(allStudents.data);
-    });
-  }, []);
+  }
+  useEffect(() =>{
+    axios.get('http://localhost:5000/apply').then( (allApplied) => {
+      TAsetStudentList(allApplied.data); 
+      console.log(allApplied.data); 
+    })
+  }, [])
 
   return (
-    <>
-      <h2> all students</h2>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="right">ID Number</TableCell>
-              <TableCell align="right">Email</TableCell>
-              <TableCell align="right">Major</TableCell>
+      <>
+      <h2>Students</h2>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell align="right">Second Name</TableCell>
+            <TableCell align="right">Last Name </TableCell>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">User Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Major</TableCell>
+            <TableCell align="right">Remove</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {TAstudentsList.map((apply,key) => (
+            <TableRow key={key}>
+              <TableCell component="th" scope="row">
+                {apply.firstName}
+              </TableCell>
+              <TableCell align="right">{apply.secondName}</TableCell>
+              <TableCell align="right">{apply.lastName}</TableCell>
+              <TableCell align="right">{apply.id}</TableCell>
+              <TableCell align="right">{apply.userName}</TableCell>
+              <TableCell align="right">{apply.section}</TableCell>
+              <TableCell align="right">{apply.major}</TableCell>
+              <TableCell align="right">
+           <IconButton aria-label="delete" className={classes.margin} onClick ={() => deleteStudent(apply._id)}> 
+          <DeleteIcon fontSize="small" />
+        </IconButton> 
+              </TableCell>
 
-              <TableCell align="right">Delete</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {TAstudentsList.map((student, key) => (
-              <TableRow key={key}>
-                <TableCell component="th" scope="row">
-                  {student.studentName}
-                </TableCell>
-            
-                <TableCell align="right">{student.id}</TableCell>
-                <TableCell align="right">{student.email}</TableCell>
-                <TableCell align="right">{student.major}</TableCell>
-                <TableCell align="right">
-                  <IconButton aria-label="delete" className={classes.margin} onClick = {() => deleteStudent(student._id)}>
-                    <DeleteIcon fontSize="small" />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </> 
   );
 }
